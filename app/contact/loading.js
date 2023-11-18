3af4eef9c01445220952cc2ffd98504dcb9f028e
Adil Abbas <adilabbas8092@gmail.com>
2023-11-18 08:54:14 -0800

    ok done

--- app/contact/loading.js
+++ app/contact/loading.js
@@ -3,7 +3,7 @@ import React from "react";
 const loading = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center text-sm">
-      loading...
+      loading....
     </div>
   );
 };
